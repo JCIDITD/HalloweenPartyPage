0d307c66dc1e68cac7a42a5884ffde1b8b378118
Spoa <justinpu02@yahoo.com>
2017-10-30 12:50:55 +0800

    bad boy update
    do stuff crazier when page unload

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,13 @@
 function whichButton(event){
   promptStuff()
 }
+
 function promptStuff(){
     input = prompt("PASSWORD:","ENTER THE PASSWORD");
     if (input == "Correct"){
         alert("HINT WORDS!");
+    else if (input == "") { // change to switch if more than 4 possible outcomes
+      alert("");  // out put hint text
     }else{
         alert("THERE IS NO ESCAPE");
     }
